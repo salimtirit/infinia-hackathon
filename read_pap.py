55f20250adecd358e61af50078d454fc83643176
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-06-17 18:39:15 +0300

    removed spaces and quotation marks from the dataframe

--- read_pap.py
+++ read_pap.py
@@ -7,9 +7,6 @@ folder_name = "Kontrol Kart 1"
 def find_table_start(csv_file):
     with open(csv_file, 'r') as file:
         for line_num, line in enumerate(file):
-            print(line_num)
-            print(line)
-            print(len(line.strip()))
             if len(line.strip().split(',')) > 1:  # Skip empty lines
                 # Check if the line can be parsed as a CSV row
                 try:
@@ -21,9 +18,8 @@ def find_table_start(csv_file):
 
 for file in os.listdir(folder_name):
     if file.endswith('.csv'):
-        print(file)
         file_path = os.path.join(folder_name, file)
-        print(file_path)
         start_line = find_table_start(file_path)
-        df = pd.read_csv(file_path,  encoding='latin1', skiprows=start_line) # TODO: skiprows=12 is a hack, find a better way
+
+        df = pd.read_csv(file_path,  encoding='latin1', skiprows=start_line,  skipinitialspace=True, quotechar='"') # TODO: skiprows=12 is a hack, find a better way
         print(df.head())
