95bef7cf5bee107b42f91a3cd36ea68b1d41b064
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-06-17 18:39:35 +0300

    get the clicked pixel and hopefully show red

--- get_clicked_pixel.py
+++ get_clicked_pixel.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def onclick(event):
     if event.button == 1:  # Left mouse button
         x, y = int(event.xdata), int(event.ydata)
         print("Clicked pixel location: ({}, {})".format(x, y))
 
-        # Change color to red (10x10 pixels)
-        image[y:y+100, x:x+100] = [255, 0, 0]  # RGB value for red
+        # Create a copy of the image to modify
+        modified_image = np.copy(image)
 
-        # Refresh the plot
-        ax.imshow(image)
+        # Change color to red (10x10 pixels)
+        modified_image[y:y+10, x:x+10] = [255, 0, 0]  # RGB value for red
+
+        # Refresh the plot with the modified image
+        ax.imshow(modified_image)
         plt.draw()
 
 # Read the image
