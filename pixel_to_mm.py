3c83c29f678ac306037a1bed791f3695eee24226
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-06-17 23:48:17 +0300

    code to function

--- pixel_to_mm.py
+++ pixel_to_mm.py
@@ -22,34 +22,37 @@ x3_mm, y3_mm = 311, 1145.75
 #right_down
 # 8192, 5464
 
-# Pixel coordinates of reference points
-pixel_coords = np.array([[x1, y1], [x2, y2], [x3, y3]], dtype=np.float32)
+def pixel_to_mm(x1, y1, x2, y2, x3, y3, x1_mm, y1_mm, x2_mm, y2_mm, x3_mm, y3_mm) -> None:
+    # Pixel coordinates of reference points
+    pixel_coords = np.array([[x1, y1], [x2, y2], [x3, y3]], dtype=np.float32)
 
-# Real-world coordinates of reference points in millimeters
-real_coords = np.array([[x1_mm, y1_mm], [x2_mm, y2_mm], [x3_mm, y3_mm]], dtype=np.float32)
+    # Real-world coordinates of reference points in millimeters
+    real_coords = np.array([[x1_mm, y1_mm], [x2_mm, y2_mm], [x3_mm, y3_mm]], dtype=np.float32)
 
-# Calculate the perspective transformation matrix
-transformation_matrix = cv2.getAffineTransform(pixel_coords, real_coords)
+    # Calculate the perspective transformation matrix
+    transformation_matrix = cv2.getAffineTransform(pixel_coords, real_coords)
 
-# Load the image
-image = cv2.imread(r'Kontrol Kart 1\5RC_6009.jpg')
+    # Load the image
+    image = cv2.imread(r'Kontrol Kart 1\5RC_6009.jpg')
 
-# Apply the perspective transformation to image coordinates
-result = cv2.warpAffine(image, transformation_matrix, (image.shape[1], image.shape[0]))
+    # Apply the perspective transformation to image coordinates
+    result = cv2.warpAffine(image, transformation_matrix, (image.shape[1], image.shape[0]))
 
-# Display the result or save it to a file
-cv2.imshow('Result', result)
-cv2.imwrite('result.jpg', result)
-cv2.waitKey(0)
+    # Display the result or save it to a file
+    cv2.imshow('Result', result)
+    cv2.imwrite('result.jpg', result)
+    cv2.waitKey(0)
 
-image_7 = cv2.imread(r'Kontrol Kart 1\5RC_6007.jpg')
+    image_7 = cv2.imread(r'Kontrol Kart 1\5RC_6007.jpg')
 
-# Apply the perspective transformation to image coordinates
-result_7 = cv2.warpAffine(image_7, transformation_matrix, (image_7.shape[1], image_7.shape[0]))
+    # Apply the perspective transformation to image coordinates
+    result_7 = cv2.warpAffine(image_7, transformation_matrix, (image_7.shape[1], image_7.shape[0]))
 
-# Display the result or save it to a file
-cv2.imshow('Result', result_7)
-cv2.imwrite('result_7.jpg', result_7)
-cv2.waitKey(0)
+    # Display the result or save it to a file
+    cv2.imshow('Result', result_7)
+    cv2.imwrite('result_7.jpg', result_7)
+    cv2.waitKey(0)
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+pixel_to_mm(x1, y1, x2, y2, x3, y3, x1_mm, y1_mm, x2_mm, y2_mm, x3_mm, y3_mm)
